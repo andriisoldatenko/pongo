6f209b0c39fae84f00146030478692ed2603becc
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-01-26 11:29:43 +0100

    init first version

---
+++ game.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"time"
+)
+
+type Game struct {
+	screen tcell.Screen
+	ball   Ball
+}
+
+func (g *Game) Run() {
+
+	defStyle := tcell.StyleDefault.Background(tcell.ColorDefault).Foreground(tcell.ColorDefault)
+	g.screen.SetStyle(defStyle)
+
+	for {
+		width, height := g.screen.Size()
+		g.ball.CheckEdges(width, height)
+
+		g.screen.Clear()
+
+		g.ball.Update()
+
+		g.screen.SetContent(g.ball.X, g.ball.Y, g.ball.Display(), nil, tcell.StyleDefault)
+		time.Sleep(40 * time.Millisecond)
+		g.screen.Show()
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"log"
+	"os"
+)
+
+func main() {
+
+	screen, err := tcell.NewScreen()
+
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	if err := screen.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	ball := Ball{
+		X:      1,
+		Y:      1,
+		Xspeed: 1,
+		Yspeed: 1,
+	}
+
+	game := Game{
+		screen: screen,
+		ball:   ball,
+	}
+
+	go game.Run()
+
+	for {
+		switch event := screen.PollEvent().(type) {
+		case *tcell.EventResize:
+			game.screen.Sync()
+		case *tcell.EventKey:
+			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
+				screen.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+
+}
